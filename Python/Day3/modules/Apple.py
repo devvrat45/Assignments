7c6277a1020be509ab53b2c04a3b34dcc5b40ba1
devvrat45 <59250172+devvrat45@users.noreply.github.com>
2020-01-08 16:57:25 +0530

    Add files via upload

---
+++ Python/Day3/modules/Apple.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+def iphone11():
+    print("This is the latest iphone")
+        
+
+
+# In[3]:
+
+
+def iphonese():
+    
+    print("This is the smallest iphone")
+
+
+# In[4]:
+
+
+def a_series():
+    print("new mid_series phone has been released")
+
+
+# In[ ]:
+
+
+
+
---
+++ Python/Day3/modules/honest.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def funca():
+    print("Hello world")
+
+
+# In[2]:
+
+
+type(funca)
+
+
+# In[3]:
+
+
+""" this can be 
+considered a module.
+"""
+
+
+# In[ ]:
+
+
+
+
